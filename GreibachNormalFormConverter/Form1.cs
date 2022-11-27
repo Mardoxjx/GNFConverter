b45859ae68becdc777a6cf8a26e301e6965b16af
Julius Reiling <80776738+Mardoxjx@users.noreply.github.com>
2022-11-27 14:47:39 +0100

    added todos

--- GreibachNormalFormConverter/Form1.cs
+++ GreibachNormalFormConverter/Form1.cs
@@ -19,6 +19,8 @@ namespace GreibachNormalFormConverter
             P_txt.Text = "S -> BC; S -> b; B -> CD; B -> ED; B -> a; C -> BC; C -> DE; D -> a; E -> b";
             S_txt.Text = "S";
             // TODO: add dropdown box with different example grammars.
+            // TODO: obsolete derivations logging incomplete.
+            // TODO: remove derivations for S in substituted derivations.
             //P_txt.Text = "Please note productions like the following: A -> x; A -> BC; B -> z";
         }
 
